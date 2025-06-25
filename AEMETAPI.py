aed453111a9445602d948ef5b9ed50d4abb983a7
EDMG3 <senorgpt2004@gmail.com>
2025-06-25 08:53:07 +0200

    Update AEMETAPI.py

--- AEMETAPI.py
+++ AEMETAPI.py
@@ -6,17 +6,16 @@ class AemetAPI:
         self.base_url = "https://opendata.aemet.es/opendata/api/"
 
     def _get_data_url(self, endpoint):
-    headers = {"Accept": "application/json", "api_key": self.api_key}
-    response = requests.get(self.base_url + endpoint, headers=headers)
-    if response.status_code == 200:
-        json_data = response.json()
-        if "datos" in json_data:
-            datos_url = json_data["datos"]
-            datos_resp = requests.get(datos_url, headers=headers)
-            if datos_resp.status_code == 200:
-                return datos_resp.json()
-    return None
-
+        headers = {"Accept": "application/json", "api_key": self.api_key}
+        response = requests.get(self.base_url + endpoint, headers=headers)
+        if response.status_code == 200:
+            json_data = response.json()
+            if "datos" in json_data:
+                datos_url = json_data["datos"]
+                datos_resp = requests.get(datos_url, headers=headers)
+                if datos_resp.status_code == 200:
+                    return datos_resp.json()
+        return None
 
     def get_prediction_city(self, city_code="08019"):
         endpoint = f"prediccion/especifica/municipio/diaria/{city_code}/"
